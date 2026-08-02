d09ed76b3929b65322f54bce1779116b324518e5
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-08-02 12:42:26 +0530

    Create basic_02_variable.cpp

---
+++ basic_02_variable.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main () {
+  int x = 5; // these are variable in which data is stored.
+  int y = 5; 
+  cout<<x+y <<endl;  // +,-,*,/. these are operator and endl means next line.
+  
+}
