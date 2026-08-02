42d17c3a02d52f6734914683c7e0bcf19257fea5
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-08-02 12:57:13 +0530

    Create basic_03_modify_value.cpp

---
+++ basic_03_modify_value.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+using namespace std;
+int main () {
+  int a = 7;
+  int b = 9;
+  a = b; // this mean a got the value of b now both have value 9.
+  cout<<a<<endl;
+  cout<<b<<endl;
+       
+}
