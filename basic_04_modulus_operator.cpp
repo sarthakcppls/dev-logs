7dc0a640c1c7a1e9ebf4eedfcb2756661a0d02cb
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-08-03 10:55:47 +0530

    Create basic_04_modulus_operator.cpp

---
+++ basic_04_modulus_operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+using namespace std;
+int main () {
+ cout<<5%2;
+  \\ this will give the remainder as a reasult.
+    \\ there properties are a%b = a if a<b
+\\ a%(-b) = a%b 
+  \\ -a%b = -(a%b)
+  
+}
