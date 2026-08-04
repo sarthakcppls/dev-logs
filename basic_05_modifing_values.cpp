ca1db567e62fad66824b36167b5e9a135c15146e
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-08-04 06:15:52 +0530

    Create basic_06_modifing_values.cpp

---
+++ basic_05_modifing_values.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+  int x = 5;
+  x+ = 5;  // technically mean x = x + 5
+  cout<<x<<endl;
+   x- = 3;
+   // technically mean x = x - 3
+  
+}
