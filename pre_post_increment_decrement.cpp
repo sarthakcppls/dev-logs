3e3b74507add5751e3e98e73193cf28adc3d1648
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-08-05 05:49:53 +0530

    Create pre_post_increment_decrement.cpp

---
+++ pre_post_increment_decrement.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 6;
+    x++;
+    cout<<x<<endl; // this mean x = x + 1 
+    // but
+    int y = 10;
+    cout<<y++<<endl; // this will not print y = y + 1 
+    // work with stand alone 
+    // it simply mean pahle print kro then add kro
+    // where as this 
+    int z = 15;
+    cout<<++z;
+    // this will print z = z + 1
+    // it simply mean pahle add kro fir print kro
+    // same goes with the (-) bss usme subtract ho jaega
+    
+}
