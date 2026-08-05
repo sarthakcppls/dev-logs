cc0457fe1f3f78fa079158992b959c014bef31cf
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-08-05 05:47:27 +0530

    Create basics_07_ascii_value.cpp

---
+++ basics_07_ascii_value.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+       char a;
+       a = 'A'; // char data type
+       cout<<a<<endl;
+       // above line will only print A
+       cout<<(int)a; // type casting in to int out put will be 
+       // 65 and 65 is ascii value of A 
+}
