01d1da085d295d98427db199de557585d72399a9
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-08-06 00:44:08 +0530

    Create relational_operator_and bool_data-type.cpp

---
+++ relational_operator_and bool_data-type.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main () {
+       // relational operator
+       int a = 5;
+       int b = 7;
+    cout<<(a==b)<<endl;
+    cout<<(a<b)<<endl;
+    cout<<(a>b)<<endl;
+    cout<<(a<=b)<<endl;
+    cout<<(a>=b)<<endl;
+    cout<<(a!=b)<<endl;
+    // these are relational operator: ==,!=, <, >, <=, >=.
+    // and these operator gives straight results eithe true or 
+    // false 
+    // and these true false value are bool data type 
+    // prints 0 if false ptint 1 if true.
+}
