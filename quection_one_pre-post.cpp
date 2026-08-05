7572bf7d334cc00b817fbf9ffb8969809b6e0aad
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-08-06 00:41:57 +0530

    Create quection_one_pre-post.cpp

---
+++ quection_one_pre-post.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+       // ek quection 
+       // int x = 8;
+       // and y = x++ + --x;
+       // agr cout kre to kya output hoga 
+       int x = 8;
+       int y;
+       y = x++ + --x;
+       cout<<y<<endl;
+       
+   
+}
