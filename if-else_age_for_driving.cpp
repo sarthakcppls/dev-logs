01cc2908db82d4d0bc5f9508f14e7a504fc177db
sarthakcppls <sarthakprakashmishra@gmail.com>
2026-08-07 07:03:13 +0530

    Create if-else_age_for_driving.cpp

---
+++ if-else_age_for_driving.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+       // if-else agr ek situation hogi to kya hoga if not to kya 
+       // kya hoga
+       
+       cout<<"enter your age: ";
+       int age;
+       cin>>age;
+       if (age>=18) cout<<"you can drive";
+       else cout<<"you cannot drive";
+}
